9ae5482ce6e6bc4f8f8086b6ecc4ba72a5bd0f71
OneDev0411 <cryptoapple1109@gmail.com>
2023-05-23 18:40:16 +0500

    help page (#8)

--- web/frontend/assets/index.js
+++ web/frontend/assets/index.js
@@ -4,6 +4,6 @@ export { default as homeImage } from "./business-plan.svg";
 export { default as offerNotFound } from "./offer-not-found.png";
 export { default as billingImg } from "./plan-upgrade.png";
 export { default as stars } from "./5-star.png";
-export { default as help } from "./Help.png";
+export { default as helpImage } from "./help.svg";
 export { default as woohoo } from "./woohoo.png";
 export {default as iculogo } from "./ICU-Logo-Small.png";
