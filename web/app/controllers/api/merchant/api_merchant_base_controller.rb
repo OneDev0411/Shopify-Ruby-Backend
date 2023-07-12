272388d54412bc618b668984de08bb4c46b8857b
OneDev0411 <cryptoapple1109@gmail.com>
2023-07-12 21:43:08 +0500

    shopify_domain param changed to shop in subscription page

--- web/app/controllers/api/merchant/api_merchant_base_controller.rb
+++ web/app/controllers/api/merchant/api_merchant_base_controller.rb
@@ -14,8 +14,8 @@ module Api
       end
 
       def find_shop
-        if params[:shopify_domain].present?
-          @icushop = Shop.find_by(shopify_domain: params['shopify_domain'])
+        if params[:shop].present?
+          @icushop = Shop.find_by(shopify_domain: params['shop'])
         else
           @icushop = Shop.find(params['shop_id'])
         end
