421705aa8f66628cf12ae004908aa9c3e0feb738
OneDev0411 <cryptoapple1109@gmail.com>
2023-07-24 23:37:02 +0500

    spceify pg config in shopify initilaizer within web folder

--- web/app/javascript/lib/shopify_app.js
+++ web/app/javascript/lib/shopify_app.js
@@ -1,9 +1,21 @@
+import {PostgreSQLSessionStorage} from '@shopify/shopify-app-session-storage-postgresql';
+
 var data = document.getElementById('shopify-app-init').dataset;
 var AppBridge = window['app-bridge'];
 var createApp = AppBridge.default;
+
+
+const sessionDb = new PostgreSQLSessionStorage.withCredentials(
+  process.env.POSTGRES_HOST,
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD
+);
+
 window.app = createApp({
   apiKey: data.apiKey,
   host: data.host,
+  SESSION_STORAGE: sessionDb
 });
 
 var actions = AppBridge.actions;
