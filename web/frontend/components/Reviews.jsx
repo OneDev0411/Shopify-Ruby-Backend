4ec297220ecbd2318fa3beb710c2d2396d7bb3fe
OneDev0411 <cryptoapple1109@gmail.com>
2023-08-07 06:51:33 -0700

    Subscription:fixed store review card sizes on subscription page (#55)
    * Subscription:fixed store review card sizes on subscription page
    
    * added seperate stylesheet for Reviews component

--- web/frontend/components/Reviews.jsx
+++ web/frontend/components/Reviews.jsx
@@ -1,14 +1,14 @@
-import {Layout, Card, Stack, Image} from "@shopify/polaris";
+import {Layout, Card, Stack, Image, HorizontalGrid} from "@shopify/polaris";
 import { useAppBridge } from '@shopify/app-bridge-react'
 import { TitleBar } from "@shopify/app-bridge-react";
 import {stars} from "../assets";
-import "../components/stylesheets/mainstyle.css";
+import "../components/stylesheets/reviewStyle.css";
 import React from 'react';
 
 export function Reviews() {
     
   return (
-    <div>
+    <div className="review-section">
       <Layout>
         <Layout.Section>
           <Stack distribution="center">
@@ -17,54 +17,64 @@ export function Reviews() {
         </Layout.Section>
       </Layout>
       <div className="space-10"></div>
-      <Layout>
-        <Layout.Section oneThird>
-          <Card title="ECOKIND Cleaning" sectioned>
-            <p><strong>Canada</strong></p>
-            <p>Great app with good features to upsell. Been using this for a month and see 
-                results. We like how it's customizable. Support is very good too!
-            </p>
-            <br/>
+      <HorizontalGrid gap="4" columns={{ xs: 1, sm: 1, md: 1, lg: 3, xl: 3 }}>
+        <Card title="ECOKIND Cleaning" sectioned>
+          <div className="card-content-wrapper">
+            <div>
+              <p>
+                <strong>Canada</strong>
+              </p>
+              <p>
+                Great app with good features to upsell. Been using this for a
+                month and see results. We like how it's customizable. Support is
+                very good too!
+              </p>
+              <br />
+            </div>
             <Stack distribution="center">
-              <Image 
-                source={stars}
-                distribution="center"
-              />
-            </Stack>      
-          </Card>
-        </Layout.Section>
-        <Layout.Section oneThird>
-          <Card title="My Gaming Case" sectioned>
-            <p><strong>United States</strong></p>
-            <p>I used to use a different in cart upsell app which had a pop up. I like how this 
-                apps upsell does not pop up. Lasandra helped me set up this app and the upsell 
-                looks beautiful in my cart page. 10/10 service, Lasandra went above & beyond!!!
-            </p>
-            <br/>
-            <Stack distribution="center">
-            <Image 
-              source={stars}
-              distribution="center"
-            />
+              <Image source={stars} distribution="center" />
             </Stack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section oneThird>
-          <Card title="Deinhamudi.de" sectioned>
-            <p><strong>Germany</strong></p>
-            <p>Very nice App with the best service! Thanks!<br/>
-                Everything works fluently and I could 3x my conversion rate. Very helpful.
-            </p>
-            <br/>
+          </div>
+        </Card>
+        <Card title="My Gaming Case" sectioned>
+          <div className="card-content-wrapper">
+            <div>
+              <p>
+                <strong>United States</strong>
+              </p>
+              <p>
+                I used to use a different in cart upsell app which had a pop up.
+                I like how this apps upsell does not pop up. Lasandra helped me
+                set up this app and the upsell looks beautiful in my cart page.
+                10/10 service, Lasandra went above & beyond!!!
+              </p>
+              <br />
+            </div>
             <Stack distribution="center">
-            <Image 
-              source={stars}
-              distribution="center"
-            />
+              <Image source={stars} distribution="center" />
             </Stack>
-          </Card>
-        </Layout.Section>
-      </Layout>
+          </div>
+        </Card>
+        <Card title="Deinhamudi.de" sectioned>
+          <div className="card-content-wrapper">
+            <div>
+              <p>
+                <strong>Germany</strong>
+              </p>
+              <p>
+                Very nice App with the best service! Thanks!
+                <br />
+                Everything works fluently and I could 3x my conversion rate.
+                Very helpful.
+              </p>
+              <br />
+            </div>
+            <Stack distribution="center">
+              <Image source={stars} distribution="center" />
+            </Stack>
+          </div>
+        </Card>
+      </HorizontalGrid>
     </div>
   );
 }
