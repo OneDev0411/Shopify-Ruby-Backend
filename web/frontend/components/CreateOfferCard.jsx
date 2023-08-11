fc5cc64ac35adeda5df052c80dff05f78178f9a9
OneDev0411 <cryptoapple1109@gmail.com>
2023-08-11 21:59:57 +0500

    adding intercom

--- web/frontend/components/CreateOfferCard.jsx
+++ web/frontend/components/CreateOfferCard.jsx
@@ -9,9 +9,10 @@ import {
   Text,
   VerticalStack, VideoThumbnail
 } from "@shopify/polaris";
-import {homeImage} from "../assets/index.js";
-import {useNavigate} from "react-router-dom";
-import {useCallback, useRef, useState} from "react";
+import { homeImage } from "../assets/index.js";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useCallback, useRef, useState } from "react";
+import { useSelector } from 'react-redux';
 
 export function CreateOfferCard() {
   const [active, setActive] = useState(false);
@@ -22,6 +23,54 @@ export function CreateOfferCard() {
   const navigateTo = useNavigate();
   const handleCreateOffer = () => {
     navigateTo('/edit-offer', { state: { offerID: null } });
+  }
+
+  const shopAndHost = useSelector(state => state.shopAndHost);
+
+  const [currentShop, setCurrentShop] = useState(null);
+  const [planName, setPlanName] = useState();
+  const [trialDays, setTrialDays] = useState();
+
+  const fetchCurrentShop = useCallback(async () => {
+
+    fetch(`/api/merchant/current_shop?shop=${shopAndHost.shop}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => { return response.json(); })
+      .then((data) => {
+        setCurrentShop(data.shop);
+        setPlanName(data.plan);
+        setTrialDays(data.days_remaining_in_trial);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      })
+  }, [setCurrentShop, setPlanName, setTrialDays]);
+
+  useEffect(() => {
+    fetchCurrentShop();
+  }, [fetchCurrentShop])
+
+  const showIntercomWidget = () => {
+    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+    var icushop = currentShop;
+    window.intercomSettings = {
+      app_id: "bzuk45ty",
+      id: icushop.id,
+      email: icushop.email,
+      phone: icushop.phone_number,
+      installed_at: icushop.created_at,
+      active: icushop.active,
+      // plan: '#{@icushop.try(:plan).try(:name)}',
+      shopify_plan: icushop.shopify_plan_name,
+      trailing_30_day_roi: icushop.trailing_30_day_roi,
+      shop_url: "https://" + icushop.shopify_domain
+      // dashboard: "https://incartupsell.herokuapp.com/?shop_id=#{@icushop.id}"
+    };
+    Intercom('show');
   }
 
   return (
@@ -56,10 +105,10 @@ export function CreateOfferCard() {
         title="Need help creating your offer?"
         primaryAction={{
           content: 'Learn more',
-          onAction: handleOpen,
+          onAction: showIntercomWidget,
         }}
-        description={"Our support team a can help walk you through it."+ "\n"+ "Chat support is open 5am to 10pm EST. Or you can send us an email anytime and we'll get back to you within 48hours."}
-        popoverActions={[{content: 'Dismiss', onAction: () => {}}]}
+        description={"Our support team a can help walk you through it." + "\n" + "Chat support is open 5am to 10pm EST. Or you can send us an email anytime and we'll get back to you within 48hours."}
+        popoverActions={[{ content: 'Dismiss', onAction: () => { } }]}
       >
         <VideoThumbnail
           onClick={handleOpen}
