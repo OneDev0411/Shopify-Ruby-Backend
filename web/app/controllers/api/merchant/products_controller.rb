af1c28a098fc2a170990d0681db6a2a935083ced
OneDev0411 <cryptoapple1109@gmail.com>
2023-11-27 22:10:55 +0000

    fix n+1 query issue in api/merchant/element_search

--- web/app/controllers/api/merchant/products_controller.rb
+++ web/app/controllers/api/merchant/products_controller.rb
@@ -6,10 +6,13 @@ module Api
       # POST /api/merchant/element_search
       def element_search
         result = product_params['type'] == 'product' ? product_search : collection_search
-        products = @icushop.products
+        shopify_ids = result.map { |hashValue| hashValue[:id] }
+        products = @icushop.products.where(shopify_id: shopify_ids).index_by(&:shopify_id)
+
         result.each do |hashedValue|
-          hashedValue[:variants] = products.find_by(shopify_id: hashedValue[:id])&.variants_details_for_searchable_product
+          hashedValue[:variants] = products[hashedValue[:id]]&.variants_details_for_searchable_product
         end
+
         render json: result
       end
 
