d76a55447bedf7732887af153c633c3bdaaafac1
OneDev0411 <cryptoapple1109@gmail.com>
2024-01-26 08:56:55 -0500

    Adjust migrations

--- web/db/migrate/20240122200444_create_shop_actions.rb
+++ web/db/migrate/20240122200444_create_shop_actions.rb
@@ -2,11 +2,11 @@ class CreateShopActions < ActiveRecord::Migration[7.0]
   def change
     create_table :shop_actions do |t|
       t.references :shop, foreign_key: true
-      t.timestamp :action_timestamp
+      t.int :action_timestamp
       t.string :shopify_domain
       t.string :action
       t.string :source
-      t.timestamps
+      t.datetime :created_at, null: false
     end
   end
 end
--- web/db/migrate/20240125111544_adjust_shop_action_times.rb
+++
@@ -1,7 +0,0 @@
-class AdjustShopActionTimes < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :shop_actions, :action_timestamp
-    add_column :shop_actions, :action_timestamp, :int
-    remove_column :shop_actions, :updated_at
-  end
-end
