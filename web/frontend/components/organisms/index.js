41b6b3a39bd12ceda3b5455c38c5cdc0917e8397
OneDev0411 <cryptoapple1109@gmail.com>
2024-03-21 15:09:22 -0400

    [Feature] 1104 Create organisms from placement tab (#392)
    * Create template for edit offer page
    
    * Create organisms from placement tab on offer edit page
    
    * Revert "Create template for edit offer page"
    
    This reverts commit a9066af59e6c815428a7391202a5b88fb39fab41.

--- web/frontend/components/organisms/index.js
+++ web/frontend/components/organisms/index.js
@@ -1,5 +1,7 @@
 import OfferProduct from "./offer-product";
 import OfferContent from "./offer-content";
 import DisplayOptions from "./display-options";
+import ChoosePlacement from "./choose-placement";
+import DisplayConditions from "./display-conditions";
 
-export { OfferProduct, OfferContent, DisplayOptions };
+export { OfferProduct, OfferContent, DisplayOptions, ChoosePlacement, DisplayConditions };
