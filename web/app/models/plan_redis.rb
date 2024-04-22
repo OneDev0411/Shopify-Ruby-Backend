6a50a9864efe0431cbc53f124bc07152b3235c1b
OneDev0411 <cryptoapple1109@gmail.com>
2024-04-22 09:58:00 -0500

    Create class to save plan data to redis

---
+++ web/app/models/plan_redis.rb
@@ -0,0 +1,78 @@
+require 'active_model'
+
+class PlanRedis
+  include ActiveModel::Validations
+
+  attr_accessor :key, :name, :price, :plan_set, :is_visible, :is_active, :created_at, :updated_at, :features
+
+  validates :key, presence: true
+
+  validates :name, presence: true
+
+  validates :price, presence: true
+
+  validates :plan_set, presence: true
+
+  validates :is_visible, inclusion: { in: [true, false] }
+
+  validates :is_active, inclusion: { in: [true, false] }
+
+  validates :created_at, presence: true
+
+  validates :updated_at, presence: true
+
+  validates :features, presence: true
+
+  def initialize(key:, price:, plan_set:, features: [], **options)
+    @key = key
+    # Pulls the string after the colon to save the name
+    @name = key[/(?<=:)(\w+)/]
+    @price = price
+    @plan_set = plan_set
+    @features = features
+    @is_visible = options[:is_visible]
+    @is_active = options[:is_active]
+    @created_at = Time.now
+    @updated_at = Time.now
+  end
+
+# Read
+# Update
+
+  def save
+    return unless valid?
+
+    flat_plan = flatten_hash_from(create_hash)
+    $redis_plans_cache.hset(key, flatten_hash_from(flat_plan))
+  end
+
+  private
+
+  def create_hash
+    hash = {}
+    self.instance_variables.each_with_object({}) do |var, hash|
+      next if var.to_s == '@key'
+
+      hash[var.to_s.delete('@')] = self.instance_variable_get(var)
+    end
+    hash
+  end
+
+  # Flattens the plan hash such that it can be saved to the redis cache
+  def flatten_hash_from(hash)
+    hash.each_with_object({}) do |(key, value), memo|
+      if value.is_a? Hash
+        next flatten_hash_from(value).each do |k, v|
+          memo["#{key}.#{k}".intern] = v
+        end
+      elsif value.is_a? Array
+        # Does not account for hashes in arrays
+        next value.each_with_index do |v, i|
+          memo["#{key}[#{i}]"] = v
+        end
+      end
+      memo[key] = value
+    end
+  end
+
+end
--- web/config/initializers/redis.rb
+++ web/config/initializers/redis.rb
@@ -4,3 +4,5 @@ require 'sidekiq-status'
 $redis = Redis.new(url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
 
 $redis_cache = Redis.new(url: ENV['REDIS_CACHE_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+
+$redis_plans_cache = Redis.new(url: ENV['REDIS_PLANS_CACHE_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
