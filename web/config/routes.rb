91cfe65c40aec2e419385c6058a7038689530d8f
OneDev0411 <cryptoapple1109@gmail.com>
2024-05-15 16:21:52 -0500

    ported prod analytics endpoints and page

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -38,6 +38,7 @@ Rails.application.routes.draw do
         post 'load_offer_details', to: 'offers#load_offer_details'
         post 'offer_settings', to: 'offers#offer_settings'
         post 'shop_settings', to: 'shops#shop_settings'
+        get 'shop_banners', to: 'shops#shop_banners'
         get 'shop_offers', to: 'offers#shop_offers'
         get '/products/shopify/:shopify_id' => 'products#shopify_details'
         get '/products/multi/:shopify_id' => 'products#details_for_multi'
@@ -63,6 +64,7 @@ Rails.application.routes.draw do
         post 'offer_activate', to: 'offers#activate'
         post 'offer_deactivate', to: 'offers#deactivate'
         post 'shop_sale_stats', to: 'shops#shop_sale_stats'
+        post 'shop_upsell_stats', to: 'shops#shop_upsell_stats'
         post 'shop_orders_stats', to: 'shops#shop_orders_stats'
 
         post 'shop_offers_stats_click_revenue', to: 'shops#shop_offers_stats_click_revenue'
--- web/frontend/pages/analytics.jsx
+++ web/frontend/pages/analytics.jsx
@@ -1,14 +1,12 @@
-import React, {useEffect} from 'react';
-import { useState, useCallback } from 'react';
+import React from 'react';
+import { useState, useCallback, useEffect } from 'react';
+import { useAuthenticatedFetch } from '../hooks';
+import { useSelector } from 'react-redux';
 import { Page ,Grid, Select, Banner } from '@shopify/polaris';
 import { AnalyticsMinor } from '@shopify/polaris-icons';
 import { GenericFooter } from '../components/GenericFooter';
 import { DateRangeOptions } from '../shared/constants/AnalyticsOptions';
 import "../components/stylesheets/mainstyle.css";
-import ModalChoosePlan from '../components/modal_ChoosePlan';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchShopData } from '../services/actions/shop';
-import { setIsSubscriptionUnpaid } from '../store/reducers/subscriptionPaidStatusSlice';
 import {
   TotalSalesData,
   TotalUpSellsData,
@@ -18,28 +16,25 @@ import {
   CustomTitleBar,
   ClickThroughtRateData
 } from "../components";
-import {useShopState} from "../contexts/ShopContext.jsx";
+import ModalChoosePlan from '../components/modal_ChoosePlan';
 import { onLCP, onFID, onCLS } from 'web-vitals';
 import { traceStat } from "../services/firebase/perf.js";
 
 export default function AnalyticsOffers() {
     const shopAndHost = useSelector((state) => state.shopAndHost);
-    const [period, setPeriod] = useState('daily');
+    const [period, setPeriod] = useState('hourly');
     const [error, setError] = useState(null);
     const [showBanner, setShowBanner] = useState(false); 
+    const [bannerData, setBannerData] = useState({ active: false });
+    const fetch = useAuthenticatedFetch(shopAndHost.host);
     const setTimePeriod = useCallback((val) => {
       setPeriod(val)
     },[]);
 
-    useEffect(()=> {
-      onLCP(traceStat, {reportSoftNavs: true});
-      onFID(traceStat, {reportSoftNavs: true});
-      onCLS(traceStat, {reportSoftNavs: true});
-    }, []);
-    
     const handleDismiss = () => {
       setError(null);
       setShowBanner(false);
+      setBannerData((bannerData)=> ({...bannerData, active: false}));
     };
 
     const handleError = () => {
@@ -47,45 +42,70 @@ export default function AnalyticsOffers() {
       setShowBanner(true);
     };
 
+    const fetchBannerData = async () => {
+      try {
+        fetch(`/api/v2/merchant/shop_banners?shop=${shopAndHost.shop}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then((response) => response.json())
+        .then((data) => setBannerData(data))
+        .catch((error) => setBannerData({ active: false }))
+      } catch (error) {
+        console.error('Error fetching banner data', error);
+      }
+    }
+
+    useEffect(()=> {
+      onLCP(traceStat, {reportSoftNavs: true});
+      onFID(traceStat, {reportSoftNavs: true});
+      onCLS(traceStat, {reportSoftNavs: true});
+      fetchBannerData();
+    }, []);
+  
     return (
       <Page>
         <ModalChoosePlan />
          <CustomTitleBar title='Analytics' icon={AnalyticsMinor} />
-          { error && showBanner && (
-            <Banner title="Data Failed To Load" onDismiss={(handleDismiss)}>
-              <p> {error} </p>
+          {bannerData?.active && (
+            <Banner title={bannerData?.title} status={bannerData?.status} onDismiss={handleDismiss}>
+              <div style={{ fontSize: bannerData?.fontSize || '0.85rem', marginTop: bannerData?.marginTop || '1rem' }}>
+                {bannerData?.lines?.map((line, index) => <p key={index} style={{ marginBottom: bannerData?.bodyMargin || '0.5rem' }}>{line}</p>)}
+              </div>
             </Banner>
           )}
         <div className="space-10"></div>
+      <Grid>
+        <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 8, lg: 4, xl: 4}}>
+          <Select
+            label="Date range"
+            options={DateRangeOptions}
+            onChange={setTimePeriod}
+            value={period}
+          />
+        </Grid.Cell>
+      </Grid>
+      <div className="space-10"></div>
+      <div id={"graphs"}>
         <Grid>
-          <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 8, lg: 4, xl: 4}}>
-            <Select
-              label="Date range"
-              options={DateRangeOptions}
-              onChange={setTimePeriod}
-              value={period}
-            />
+          <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 8, lg: 4, xl: 4 }}>
+            <TotalSalesData period={period} onError={handleError} />
+            <ClickThroughtRateData period={period} onError={handleError}  />
+          </Grid.Cell>
+          <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 8, lg: 4, xl: 4 }}>
+            <TotalUpSellsData period={period} onError={handleError} />
+            <ConversionRate period={period } onError={handleError} />
+          </Grid.Cell>
+          <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 8, lg: 4, xl: 4 }}>
+            <OrderOverTimeData period={period} onError={handleError} />
+            <TopPerformingOffersData period={period} onError={handleError} />
           </Grid.Cell>
         </Grid>
-        <div className="space-10"></div>
-        <div id={"graphs"}>
-          <Grid>
-            <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 8, lg: 4, xl: 4 }}>
-              <TotalSalesData period={period} onError={handleError} />
-              <ClickThroughtRateData period={period} onError={handleError}  />
-            </Grid.Cell>
-            <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 8, lg: 4, xl: 4 }}>
-              <TotalUpSellsData period={period} onError={handleError} />
-              <ConversionRate period={period } onError={handleError} />
-            </Grid.Cell>
-            <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 8, lg: 4, xl: 4 }}>
-              <OrderOverTimeData period={period} onError={handleError} />
-              <TopPerformingOffersData period={period} onError={handleError} />
-            </Grid.Cell>
-          </Grid>
-        </div>
-        <div className='space-10'></div>
-        <GenericFooter text="Learn more about " linkUrl="#" linkText="analytics" />
-      </Page>
+      </div>
+      <div className='space-10'></div>
+      <GenericFooter text="Learn more about " linkUrl="#" linkText="analytics" />
+    </Page>  
   );
 }
